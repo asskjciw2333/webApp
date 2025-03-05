69efd96dbad3790707021065b1b4aeeaa06bebfc
GH23473 <GH23473@Surf.com>
2025-03-05 11:20:47 +0200

    Removed ignored files from tracking

--- omegaApp/static/js/panels/index.js
+++
@@ -1,9 +0,0 @@
-import { initializeApp } from './panels-main.js';
-import { PortsStatusManager } from './ports-status.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-    initializeApp();
-    
-    // Initialize ports status functionality
-    new PortsStatusManager();
-}); 
--- webpack.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    resolve: {
-        alias: {
-            'three': path.resolve('./node_modules/three')
-        }
-    }
-} 
